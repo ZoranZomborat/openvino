1c23467139ede84c65eb25903ea365dcfde44576
Zoran Zomborat <zoran.zomborat@intel.com>
2021-01-26 16:00:33 +0200

    try out the case of 4 inference requests on OV jenkins

--- inference-engine/samples/benchmark_app/main.cpp
+++ inference-engine/samples/benchmark_app/main.cpp
@@ -131,9 +131,7 @@ int main(int argc, char *argv[]) {
                 command_line_arguments.push_back({ flag.name, flag.current_value });
             }
         }
-        // FLAGS_api = "sync";
-        FLAGS_t = 2;
-        FLAGS_nireq = 1;
+        FLAGS_nireq = 4;
         if (!FLAGS_report_type.empty()) {
             statistics = std::make_shared<StatisticsReport>(StatisticsReport::Config{FLAGS_report_type, FLAGS_report_folder});
             statistics->addParameters(StatisticsReport::Category::COMMAND_LINE_PARAMETERS, command_line_arguments);
